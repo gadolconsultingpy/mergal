0afcc99c577ae690e2a81cf27a8cf0c4ccba2b86
Martin Salcedo <martinsalcedo.apps@gmail.com>
2025-10-27 17:35:28 -0300

    [IMP] Updates and translates terms in es_PY
    Updates the translation file for the l10n_py module and adds a new translation file for l10n_py_purchase_stock.
    
    - Updates POT-Creation-Date and PO-Revision-Date
    - Updates Last-Translator
    - Updates X-Generator
    - Translates new terms, corrects existing translations, and removes obsolete entries.
    - Adds computed field for inverse rate in purchase orders

--- extra-addons/l10n_py/models/purchase_order.py
+++ extra-addons/l10n_py/models/purchase_order.py
@@ -6,7 +6,22 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     invoice_currency_rate = fields.Float(string='Invoice Currency Rate', compute="_compute_invoice_currency_rate", )
-    inverse_rate = fields.Float(string='User Inverse Rate', digits=(12, 6), default=1.0)
+    inverse_rate = fields.Float(string='User Inverse Rate', digits=(12, 6), default=1.0,
+                                compute="_compute_inverse_rate", store=True)
+
+    @api.depends('currency_id', 'company_currency_id', 'company_id', 'date_order')
+    def _compute_inverse_rate(self):
+        for porder in self:
+            if porder.currency_id != porder.company_id.currency_id:
+                currency_rate = self.env['res.currency']._get_conversion_rate(
+                        from_currency=porder.company_currency_id,
+                        to_currency=porder.currency_id,
+                        company=porder.company_id,
+                        date=porder._get_purchase_order_currency_rate_date(),
+                )
+            else:
+                currency_rate = 1
+            porder.inverse_rate = 1.0 / currency_rate or 1.0
 
     @api.depends('invoice_currency_rate', 'inverse_rate', 'currency_id')
     def _compute_invoice_currency_rate(self):
@@ -19,3 +34,6 @@ class PurchaseOrder(models.Model):
                     rec.invoice_currency_rate = 1.0 / rec.inverse_rate or 1.0
             else:
                 rec.invoice_currency_rate = 1.0
+
+    def _get_purchase_order_currency_rate_date(self):
+        return self.date_order or fields.Date.today()
