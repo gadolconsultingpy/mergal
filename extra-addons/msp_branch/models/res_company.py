35da3f3a7e9fb777492043fe9c79da54780a9121
Martin Salcedo <martinsalcedo.apps@gmail.com>
2025-10-26 23:21:53 -0300

    [DEL] Removes msp_account_branch and msp_branch modules
    Removes the msp_account_branch and msp_branch modules.
    These modules are no longer required in the codebase.

--- extra-addons/msp_branch/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, models, fields
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    def get_default_branch(self):
-        branch = self.env['res.branch'].search([('company_id','=',self.id)], order="sequence",limit=1)
-        return branch
