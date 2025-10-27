7ddc5b8efedd3b960ece50c3c94b9981b71aee21
Martin Salcedo <martinsalcedo.apps@gmail.com>
2025-10-26 23:26:29 -0300

    [IMP] Updates Odoo Docker script.
    Updates the Odoo Docker container configuration to use the official Odoo image and simplifies volume mounts.
    
    - Switches to the official Odoo image (`odoo:18.1`).
    - Renames the `docker_odoo` volume mount to `odoo` for clarity.
    - Enables some base module updates.

--- server_odoo8018ee.py
+++ server_odoo8018ee.py
@@ -1,13 +1,13 @@
 from docker import DockerContainer
 import os
 
-ctnr = DockerContainer('odoo18mergal', 'martinsalcedoapps/odoo:18.2')
+ctnr = DockerContainer('odoo18mergal', 'odoo:18.1')
 ctnr.set_localpath(os.path.abspath('.'))
 ctnr.add_volume('odoo18ee.conf', '/etc/odoo/odoo.conf')
 ctnr.add_volume('enterprise', '/mnt/enterprise')
 ctnr.add_volume('extra-addons', '/mnt/extra-addons')
 ctnr.add_volume('var_lib_odoo', '/var/lib/odoo')
-ctnr.add_volume('docker_odoo', '/mnt/docker')
+ctnr.add_volume('odoo', '/mnt/odoo')
 ctnr.add_port(8018, 8069)
 ctnr.add_link("db5018", 'db')
 
@@ -15,6 +15,7 @@ ctnr.add_link("db5018", 'db')
 # ctnr.add_extra_command("-u msp_account_payment_sheet")
 # ctnr.add_extra_command("-u l10n_py_edi")
 # ctnr.add_extra_command("-u msp_customs_management_analytic")
-# ctnr.add_extra_command("-u base")
+# ctnr.add_extra_command("-u account")
+# ctnr.add_extra_command("-u l10n_py")
 
 ctnr.run(stop_and_rebuild=True)
